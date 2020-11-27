4632bb6b903b06e4257a16b785e070a24e947004
Martin Šmíd <cyberklezmer@gmail.com>
2020-11-27 21:35:38 +0100

    switch

--- src/seir.hpp
+++ src/seir.hpp
@@ -302,6 +302,11 @@ public:
         for(; i<= t+ds; i++)
         {
             dmatrix& T = res.Ts[i-1];
+
+Eigen::IOFormat f(Eigen::StreamPrecision, Eigen::DontAlignCols,",");
+cerr << "time " << i << endl << T.format(f) << endl << endl << endl;
+if(i>300)
+    throw;
             dmatrix THT = stackm(T,H*T);
 
             dvector N;
